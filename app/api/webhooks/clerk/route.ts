fa8642a615353805960c5afbf0c858bf42cc874a
enoch-promise <enochpromise.va@gmail.com>
2024-02-24 12:09:12 -0800

    FIRST COMMIT

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -69,6 +69,7 @@ export async function POST(req: Request) {
       lastName: last_name,
       photo: image_url,
     };
+    console.log("🚀 ~ POST ~ user:", user)
 
     const newUser = await createUser(user);
 
