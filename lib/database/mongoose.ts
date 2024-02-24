f4a44fe5effcd51c464ced4a8ff53d89fd4770d2
enoch-promise <enochpromise.va@gmail.com>
2024-02-24 15:00:26 -0800

    commit

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -23,7 +23,7 @@ export const connectToDatabase = async () => {
   cached.promise = 
     cached.promise || 
     mongoose.connect(MONGODB_URL, { 
-      dbName: 'imaginify', bufferCommands: false 
+      dbName: 'Ai_image', bufferCommands: false 
     })
 
   cached.conn = await cached.promise;
